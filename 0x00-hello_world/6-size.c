9e02a73e149a8c4f3e65151309b55fb2f813fa9f
EnochOluyemi <morijin1246@gmail.com>
2022-09-08 18:12:06 -0700

    My first program in c lang

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("%s%lu%s\n", "Size of a char: ", sizeof(char), " byte(s)");
 	printf("%s%lu%s\n", "Size of an int: ", sizeof(int), " byte(s)");
 	printf("%s%lu%s\n", "Size of a long int: ", sizeof(long), " byte(s)");
-	printf("%s%lu%s\n", "Size of a long long int: ", sizeof(long long), " byte(s)");
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("%s%lu%s\n", "Size of a float: ", sizeof(float), " byte(s)");
 	return (0);
 }
