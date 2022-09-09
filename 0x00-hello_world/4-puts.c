d909054c947ab4f49d15ad493f3a4d5ff56e487a
EnochOluyemi <morijin1246@gmail.com>
2022-09-08 17:55:02 -0700

    My first program in c lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle,"
+ * use the function puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char c[] = "Programming is like building a multilingual puzzle";
-
-        puts(c);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
