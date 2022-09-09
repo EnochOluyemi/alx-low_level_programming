f367f7aa7019a34d7f90256f0ac5e4cc8776a67c
EnochOluyemi <morijin1246@gmail.com>
2022-09-08 17:16:58 -0700

    fix

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints Hello world.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
 {
+	int c;
 	int c[] = "Hello world";
+
 	printf("%s\n", c);
-	return 0;
+	return (0);
 }
