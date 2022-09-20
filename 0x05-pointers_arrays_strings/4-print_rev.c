96c39276b7d67b0f91662a422b5c819cb6f4a419
EnochOluyemi <morijin1246@gmail.com>
2022-09-19 18:26:56 -0700

    pointers,arrays & strings tasks done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,12 @@
 
 void print_rev(char *str)
 {
-        int i, len = strlen(str);
+	int i, len = strlen(str);
 
 	for (i = len - 1; i >= 0; i--)
+	{
 		_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
